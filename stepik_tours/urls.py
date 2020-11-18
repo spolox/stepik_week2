824274d3b123a9ea8ea9a196ce61bddb6bb222e6
Spolox <draconvs@yandex.ru>
2020-11-18 18:52:27 +0300

    Fixed some bugs

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+
 from tours.views import custom_handler404, custom_handler500
 
 
